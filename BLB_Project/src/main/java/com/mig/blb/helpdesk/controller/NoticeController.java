38041fa90ff87bc828e96e46da5180639cd900ab
shininvvs <jack40553@gmail.com>
2024-12-05 12:55:31 +0900

    테스트
    테스트

--- BLB_Project/src/main/java/com/mig/blb/helpdesk/controller/NoticeController.java
+++ BLB_Project/src/main/java/com/mig/blb/helpdesk/controller/NoticeController.java
@@ -13,29 +13,31 @@ import com.mig.blb.common.template.Pagination;
 import com.mig.blb.helpdesk.model.service.NoticeService.NoticeService;
 import com.mig.blb.helpdesk.model.vo.Notice;
 
-@Controller
-public class NoticeController {
-
-	@Autowired
-	private NoticeService noticeService;
+	@Controller
+	public class NoticeController {
 	
-	@GetMapping("list.no")
-	public String selectList(@RequestParam(value="cpage", defaultValue="1")int currentPage,
-							 Model model) {
+		@Autowired
+		private NoticeService noticeService;
 		
-		int listCount = noticeService.selectListCount();
-		
-		int pageLimit = 5;
-		int boardLimit = 5;
-		
-		PageInfo pi = Pagination.getPageInfo(listCount, currentPage, pageLimit, boardLimit);
-		ArrayList<Notice> list = noticeService.selectList(pi);
-		
-		model.addAttribute("list",list);
-		model.addAttribute("pi",pi);
-		
-		return "helpdesk/CustomerServiceCenter";
+		@GetMapping("list.no")
+		public String selectList(@RequestParam(value="cpage", defaultValue="1")int currentPage,
+								 Model model) {
+			
+			int listCount = noticeService.selectListCount();
+			
+			int pageLimit = 5;
+			int boardLimit = 5;
+			
+			PageInfo pi = Pagination.getPageInfo(listCount, currentPage, pageLimit, boardLimit);
+			ArrayList<Notice> list = noticeService.selectList(pi);
+			
+			model.addAttribute("list",list);
+			model.addAttribute("pi",pi);
+			
+			return "helpdesk/CustomerServiceCenter";
+			
+		}
+	
+	public 
 		
 	}
-	
-}
