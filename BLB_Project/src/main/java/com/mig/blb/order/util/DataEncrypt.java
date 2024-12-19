47af09c14b231390cbe544b50bf70ac3df861158
yoonprincess <yewon9751@gmail.com>
2024-12-19 18:27:34 +0900

    나이스페이 환경 구축 - 예원
    DataEncrypt 폴더 및 클래스 파일 생성
    pom.xml 의존성 추가

---
+++ BLB_Project/src/main/java/com/mig/blb/order/util/DataEncrypt.java
@@ -0,0 +1,33 @@
+package com.mig.blb.order.util;
+
+import java.security.MessageDigest;
+import org.apache.commons.codec.binary.Hex;
+
+// SHA-256 형식으로 암호화
+public class DataEncrypt{
+	MessageDigest md;
+	String strSRCData = "";
+	String strENCData = "";
+	String strOUTData = "";
+	
+	public DataEncrypt(){ }
+	public String encrypt(String strData){
+		String passACL = null;
+		MessageDigest md = null;
+		try{
+			md = MessageDigest.getInstance("SHA-256");
+			md.reset();
+			md.update(strData.getBytes());
+			byte[] raw = md.digest();
+			passACL = encodeHex(raw);
+		}catch(Exception e){
+			System.out.print("암호화 에러" + e.toString());
+		}
+		return passACL;
+	}
+	
+	public String encodeHex(byte [] b){
+		char [] c = Hex.encodeHex(b);
+		return new String(c);
+	}
+}
